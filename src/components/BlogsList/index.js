83010e84a9a54dcbdbbd835239a7d98cacd2fb41
GowriChelluri <gowrichelluri08@gmail.com>
2024-02-19 07:54:09 +0000

    Commited

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,43 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsList: [], isLoading: true}
+
+  componentDidMount() {
+    this.onFetchUrl()
+  }
+
+  onFetchUrl = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsList: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsList, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsList.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
